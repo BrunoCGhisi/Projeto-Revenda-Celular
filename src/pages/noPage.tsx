cbb7407b01815d6cb0e890e7ec9f7ae9b3b047b9
BrunoCGhisi <brunocostaghisi@gmail.com>
2024-08-14 15:22:27 -0300

    init front

---
+++ src/pages/noPage.tsx
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+
+const NoPage = () => {
+    return (
+    <div> 
+        <h1> Aparentemente você digitou alguma coisa errada! </h1>
+        <h2> Ou só não criamos essa pagína ainda </h2>
+        <h3> Nosso front-end está passando fome... </h3>
+    </div>
+    )
+}
+
+
+export default NoPage
---
+++ src/router/Router.tsx
@@ -0,0 +1,20 @@
+import React, { BrowserRouter, Route, Routes } from "react-router-dom";
+
+//importando todas as páginas
+import { Home } from "../pages";
+import { NoPage } from "../pages";
+
+//importando todas as paginas que foram exportadas no ../pages/index.ts
+
+const Router = () => (
+  //constante Router é igual a uma função vazia que vai executar a pesquisa rotas e paginas
+  <BrowserRouter>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="*" element={<NoPage />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default Router;
